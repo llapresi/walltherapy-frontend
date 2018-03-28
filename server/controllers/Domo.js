8cfcdc18e827fccd12808f721ff4930ee3b8a041
llapresi <lxl6996@rit.edu>
2018-03-28 11:16:39 -0400

    Added favFood requirement to makeDomo

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -14,8 +14,8 @@ const makerPage = (req, res) => {
 };
 
 const makeDomo = (req, res) => {
-  if (!req.body.name || !req.body.age) {
-    return res.status(400).json({ error: 'RAWR! Both name and age are required' });
+  if (!req.body.name || !req.body.age || !req.body.favFood) {
+    return res.status(400).json({ error: 'RAWR! All fields are required' });
   }
 
   const domoData = {
