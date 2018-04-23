7bf31d9be5466d21eaa46cf980b3c3ded525f467
llapresi <lxl6996@rit.edu>
2018-04-23 13:55:24 -0400

    fixed https redirect

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -14,7 +14,7 @@ const requiresLogout = (req, res, next) => {
 
 const requiresSecure = (req, res, next) => {
   if (req.headers['x-forwarded-proto'] !== 'https') {
-    return res.redirect(`https://https${req.hostname}${req.url}`);
+    return res.redirect(`https://${req.hostname}${req.url}`);
   }
   return next();
 };
