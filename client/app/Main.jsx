4ca73ac19243a0806c5667d640c32615d496692f
llapresi <lxl6996@rit.edu>
2018-09-17 14:45:01 -0400

    using https for tile url now

--- client/app/Main.jsx
+++ client/app/Main.jsx
@@ -310,7 +310,7 @@ class App extends React.Component {
             >
               <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+                url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
               />
               <AttributionControl position="topright" />
               {
