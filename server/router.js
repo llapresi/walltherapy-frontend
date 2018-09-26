b27e6d1d950cb26f7dd9faa0c662319482b2de5f
llapresi <lxl6996@rit.edu>
2018-09-26 12:57:38 -0400

    Server router now redirects root to '/app' directory. No more standalone login TODO: Fix snackbars, remove old login files

--- server/router.js
+++ server/router.js
@@ -2,7 +2,6 @@ const controllers = require('./controllers');
 const mid = require('./middleware');
 
 const router = (app) => {
-  app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
   app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
   app.get('/getToken', mid.requiresSecure, controllers.Account.getToken);
   app.get('/spots', controllers.Spot.getSpots);
@@ -11,8 +10,10 @@ const router = (app) => {
   app.post('/changePassword', mid.requiresLogin, mid.requiresSecure,
     controllers.Account.changePassword);
   app.get('/logout', mid.requiresLogin, controllers.Account.logout);
-  app.get('/app*', controllers.App.appPage);
-  app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
+  app.get('/app*', mid.requiresSecure, controllers.App.appPage);
+  app.get('/', mid.requiresSecure, mid.requiresLogout, (req, res) => {
+    res.redirect('/app');
+  });
   app.post('/reviews', mid.requiresLogin, controllers.Review.addReview);
   app.get('/reviews', controllers.Review.getReviews);
   app.get('/getUsernameForId', mid.requiresLogin, controllers.Account.getUsernameForId);
