db4629d8a27f6feed9dfc59bd8a578bbcd3f0633
Luke Lapresi <lxl6996@rit.edu>
2018-09-27 20:36:52 -0400

    Added clean step + PWA manifest

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,12 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { GenerateSW } = require('workbox-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin'); //installed via npm
 const webpack = require('webpack');
 
 module.exports = {
   plugins: [
+    new CleanWebpackPlugin('./dist'),
     new webpack.DefinePlugin({ // <-- key to reducing React's size
       'process.env': {
         NODE_ENV: JSON.stringify('production'),
@@ -13,7 +15,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
-      filename: '[name].css',
+      filename: '[name].[contenthash].css',
       chunkFilename: '[id].css',
     }),
     new HtmlWebpackPlugin({
@@ -52,7 +54,7 @@ module.exports = {
   output: {
     path: `${__dirname}/dist`,
     publicPath: '/app/',
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
   },
   devServer: {
     publicPath: '/app/',
